32cface65311a06dfaba6c55bef11e74a4a6e8ce
Sibidharan <sibi@selfmade.ninja>
2026-02-20 05:15:37 +0530

    Initial Commit

---
+++ Projector/ProjectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectorApp.swift
+//  Projector
+//
+//  Created by Sibidharan on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
